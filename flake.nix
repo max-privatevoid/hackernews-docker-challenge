1cfcd03abaee38e44bd0ca422920750d6cf69ead
Max <max@privatevoid.net>
2024-03-16 13:04:28 +0100

    simplify

--- flake.nix
+++ flake.nix
@@ -3,46 +3,61 @@
 
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    flake-utils.url = "github:numtide/flake-utils"; 
   };
 
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      let 
-        app = pkgs.stdenv.mkDerivation rec {
-          name = "app";
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        jdk = pkgs.jdk21_headless;
+
+        jre = pkgs.jre_minimal.override {
+          inherit jdk;
+          modules = [
+            "java.base"
+            "java.desktop"
+            "java.logging"
+            "java.management"
+            "java.naming"
+            "java.security.jgss"
+            "java.instrument"
+          ];
+        };
+
+        app = pkgs.stdenvNoCC.mkDerivation {
+          pname = "app";
           version = "1.0.0";
-          src = ./.;
-          buildInputs = [ pkgs.zulu pkgs.makeWrapper ];
-          nativeBuildInputs = [ pkgs.removeReferencesTo ];
-          installPhase = ''
+
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+
+          buildCommand = ''
             mkdir -pv $out/share/java $out/bin
-            cp ${src}/build/libs/demo-0.0.1-SNAPSHOT.jar $out/share/java/${name}.jar
-            makeWrapper ${pkgs.zulu}/bin/java $out/bin/app --add-flags "-jar $out/share/java/${name}.jar" 
+            cp ${./build/libs/demo-0.0.1-SNAPSHOT.jar} $out/share/java/$pname.jar
+            makeWrapper ${jre}/bin/java $out/bin/app --add-flags "-jar $out/share/java/$pname.jar" 
           '';
         };
-        dockerImage = pkgs.dockerTools.buildImage {
-            name = "docker-challange-image";
-            tag = "latest";
-            copyToRoot = [ app ];
-            config = {
-              Cmd = [ "${app}/bin/app" ];
-            };
-          };
-      in 
-      rec {
-        packages.app = app;
-        packages.dockerImage = dockerImage;
 
-        defaultPackage = packages.app;
-        devShell = pkgs.mkShell {
-          buildInputs = [ pkgs.zulu pkgs.gradle ];
+        dockerImage = pkgs.dockerTools.buildLayeredImage {
+          name = "docker-challenge-image";
+
+          config.Cmd = [ "${app}/bin/app" ];
+
+          fakeRootCommands = ''
+            install -dm 1777 tmp
+          '';
+        };
+      in 
+      {
+        packages = {
+          inherit app dockerImage;
+          default = app;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ jdk pkgs.gradle ];
         };
       }
     );
